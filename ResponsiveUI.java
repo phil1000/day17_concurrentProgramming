b3b0980fce2d886aaffa86542de5d14b1f2c77d2
unknown <pmoura01@cspc050.dcs.bbk.ac.uk>
2013-11-27 16:50:49 +0000

    minor modifications

--- ResponsiveUI.java
+++ ResponsiveUI.java
@@ -1,24 +1,23 @@
 public class ResponsiveUI implements Runnable {
 
-	private Counter taskNo;
-	public ResponsiveUI(Counter counter) {
-		this.taskNo = counter;
+	private long duration;
+	public ResponsiveUI(long duration) {
+		this.duration = duration;
 	}
 	
 	public void run() {
 		// this is where the logic to be threaded goes
-		System.out.println("Finished task " + taskNo.getCount());
+		Thread.sleep(this.duration);
+		System.out.println("Finished task " + this.duration);
 	}
 	
 	public static void main(String[] args) {
-		Counter counter = new Counter();
 		for (int i=0;i<10;i++) {
 			System.out.println("Enter the duration (in ms) of task " + i);
-			String duration = System.console().readLine();
-			Runnable r = new ResponsiveUI(counter);
+			Long duration = Long.parseLong(System.console().readLine());
+			Runnable r = new ResponsiveUI(duration);
 			Thread t = new Thread(r);
 			t.start();
-			counter.increase();
 		}
 	}
 }
